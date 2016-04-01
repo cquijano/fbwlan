b45bf09fa7b34327f2ae88a01d556f87d6fa1e9a
Carlos Quijano <carlos@crqgestion.es>
2016-04-01 10:17:48 +0200

    [FEATURE] Best use relative path , avoid cross domains errors when using https/http, now can be used both

--- views/head.php
+++ views/head.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html>
     <head>
-        <link rel="stylesheet" href="<?php echo $my_url; ?>/static/pure-min.css">
-        <link rel="stylesheet" href="<?php echo $my_url; ?>/static/grids-responsive-min.css">
-        <link rel="stylesheet" href="<?php echo $my_url; ?>/static/fontawesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="<?php echo $my_url; ?>/static/my.css">
+        <link rel="stylesheet" href="static/pure-min.css">
+        <link rel="stylesheet" href="static/grids-responsive-min.css">
+        <link rel="stylesheet" href="static/fontawesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="static/my.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php echo $title; ?></title>
     </head>
@@ -14,7 +14,7 @@
         <div class="pure-u-1">
             <h1><?php echo $title; ?></h1>
             <!-- .pure-img makes images responsive -->
-            <img class="pure-img" src="<?php echo $my_url; ?>static/logo.jpg"/>
+            <img class="pure-img" src="static/logo.jpg"/>
         </div>
     </div>
 
